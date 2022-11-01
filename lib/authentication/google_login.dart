313cf4696610af097548ef88aa8197c3d1128f3b
Vinit <connect2jc@gmail.com>
2022-11-01 12:15:59 +0530

    Google Login bug fix

--- lib/authentication/google_login.dart
+++ lib/authentication/google_login.dart
@@ -8,24 +8,29 @@ mixin GoogleLogin {
     // Trigger the authentication flow
     try {
       final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
-      // Obtain the auth details from the request
-      final GoogleSignInAuthentication? googleAuth =
-          await googleUser?.authentication;
-      // Create a new credential
-      final credential = GoogleAuthProvider.credential(
-        accessToken: googleAuth?.accessToken,
-        idToken: googleAuth?.idToken,
-      );
-      try {
-        UserCredential userCredential =
-            await firebaseInstance.signInWithCredential(credential);
-        Mode().changeLoginMode = LoginMode.google;
-        onSuccess(
-            '${userCredential.user?.displayName ?? ''} Logged in successfully',
-            userCredential);
-      } on FirebaseAuthException catch (e) {
-        String errorMeessage = ExceptionHandlingHelper.handleException(e.code);
-        onError(errorMeessage);
+      if (googleUser != null) {
+        // Obtain the auth details from the request
+        final GoogleSignInAuthentication googleAuth =
+            await googleUser.authentication;
+        // Create a new credential
+        final credential = GoogleAuthProvider.credential(
+          accessToken: googleAuth.accessToken,
+          idToken: googleAuth.idToken,
+        );
+        try {
+          UserCredential userCredential =
+              await firebaseInstance.signInWithCredential(credential);
+          Mode().changeLoginMode = LoginMode.google;
+          onSuccess(
+              '${userCredential.user?.displayName ?? ''} Logged in successfully',
+              userCredential);
+        } on FirebaseAuthException catch (e) {
+          String errorMeessage =
+              ExceptionHandlingHelper.handleException(e.code);
+          onError(errorMeessage);
+        }
+      } else {
+        onError('Google Signup/Login cancelled');
       }
     } on PlatformException catch (e) {
       logger.e("Google Platform Exception", e.toString());
